ddccd16cfa683824e58bef0a948abeb0595ae41c
Yakeen Zreba <87274102+Yakeen-Zreba@users.noreply.github.com>
2023-12-04 12:26:03 +0200

    change home to hi5

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,7 +15,7 @@ class HomePage extends StatelessWidget {
             .of(context)
             .colorScheme
             .inversePrimary,
-        title: const Text('Hi4'),
+        title: const Text('Hi5'),
       ),
       body: BlocBuilder<InternetBloc, InternetState>(
         builder: (context, state) {
